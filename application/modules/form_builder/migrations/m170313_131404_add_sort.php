8602e2afde580e9dadd905ba8be16472f04e11c0
Volodymyr Hryvinskyi <script@email.ua>
2017-03-13 15:26:02 +0200

    fix colun sort migration

--- application/modules/form_builder/migrations/m170313_131404_add_sort.php
+++ application/modules/form_builder/migrations/m170313_131404_add_sort.php
@@ -7,12 +7,12 @@ class m170313_131404_add_sort extends Migration
 {
     public function up()
     {
-        $this->addColumn('{{%form_builder_forms}}', 'sort', Schema::TYPE_INTEGER);
+        $this->addColumn('{{%form_builder_field}}', 'sort', Schema::TYPE_INTEGER);
     }
 
     public function down()
     {
-        $this->dropColumn('{{%form_builder_forms}}', 'sort');
+        $this->dropColumn('{{%form_builder_field}}', 'sort');
 
         return false;
     }
