fbdf07f76888c2eb0abfe5ccc8eaf8ca300533c7
Volodymyr Hryvinskyi <script@email.ua>
2017-03-24 19:48:04 +0200

    add 2 methot for migration modules

--- application/commands/MigrateController.php
+++ application/commands/MigrateController.php
@@ -39,11 +39,34 @@ class MigrateController extends \yii\console\controllers\MigrateController
             $this->db->schemaCache->flush();
         }
 
-        $this->migrationNamespaces['log'] = 'yii\log\migrations';
-        $this->migrationNamespaces['rbac'] = 'yii\rbac\migrations';
+        $this->migrationNamespaces['yii-log'] = 'yii\log\migrations';
+        $this->migrationNamespaces['yii-rbac'] = 'yii\rbac\migrations';
 
         $this->attachModuleMigrations();
         return true;
+    }
+
+    /**
+     * Migrates current module up.
+     *
+     * @param string         $module module name.
+     * @param string|integer $limit  migrations limit.
+     */
+    public function actionModuleUp($module, $limit = 'all')
+    {
+        $this->attachModuleMigrations($module);
+        parent::actionUp($limit);
+    }
+    /**
+     * Migrates current module down.
+     *
+     * @param string         $module module name.
+     * @param string|integer $limit  migrations limit.
+     */
+    public function actionModuleDown($module, $limit = 'all')
+    {
+        $this->attachModuleMigrations($module);
+        parent::actionDown($limit);
     }
 
     /**
@@ -56,7 +79,7 @@ class MigrateController extends \yii\console\controllers\MigrateController
         if (strpos($class, '\\') === false) {
             $class = $this->namespace . '\\' . $class;
         } elseif (strpos($class, 'yii\\') !== false) {
-            $path = $this->getPathNamespace($class);
+            $path = $this->getNamespacePath($class);
             $file = $path . '.php';
 
             $class = explode(DIRECTORY_SEPARATOR, $path);
@@ -72,22 +95,31 @@ class MigrateController extends \yii\console\controllers\MigrateController
      * Returns the file path matching the give namespace.
      * @param string $namespace namespace.
      * @return string file path.
+     * @since 2.0.10
      */
-    private function getPathNamespace($namespace)
+    private function getNamespacePath($namespace)
     {
         return str_replace('/', DIRECTORY_SEPARATOR, Yii::getAlias('@' . str_replace('\\', '/', $namespace)));
     }
 
     /**
+     * @param $module string|null
      * creates $migrationNamespaces attribute from module base paths
      */
-    protected function attachModuleMigrations()
+    protected function attachModuleMigrations($module = null)
     {
         $migrationNamespaces = [];
         foreach (Yii::$app->modules as $name => $config) {
             $namespace = new \ReflectionClass(get_class(Yii::$app->getModule($name)));
             $migrationNamespaces[$name] = $namespace->getNamespaceName() . '\\' . 'migrations';
         }
-        $this->migrationNamespaces += $migrationNamespaces;
+
+        if($module === null) {
+            $this->migrationNamespaces = $migrationNamespaces;
+        } elseif (isset($this->migrationNamespaces[$module])) {
+            $this->migrationNamespaces = [$module => $this->migrationNamespaces[$module]];
+        } else {
+            $this->migrationNamespaces = [];
+        }
     }
 }
