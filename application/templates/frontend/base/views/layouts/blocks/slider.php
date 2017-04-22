bf621072d75916e7876da4cdb4a16176f6da6e41
Volodymyr Hryvinskyi <script@email.ua>
2017-04-22 11:35:52 +0300

    .gitignore update files

--- application/templates/frontend/base/views/layouts/blocks/slider.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Created by Vladimir Hryvinskyy.
- * Site: http://codice.in.ua/
- * Date: 12.10.2016
- * Project: kotsyubynsk
- * File name: slider.php
- */
-echo \app\modules\text\api\Text::get('slider');
