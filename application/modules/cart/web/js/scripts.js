b06620f92b5dca3b47e0bfaea05101d0d8740c50
Volodymyr Hryvinskyi <script@email.ua>
2017-05-17 20:51:58 +0300

    fix jquery typescript

--- application/modules/cart/web/js/scripts.js
+++ application/modules/cart/web/js/scripts.js
@@ -1,6 +1,3 @@
-"use strict";
-exports.__esModule = true;
-var $ = require("jquery");
 var OakCMS;
 (function (OakCMS) {
     var Cart = (function () {
--- application/modules/cart/web/js/scripts.ts
+++ application/modules/cart/web/js/scripts.ts
@@ -1,4 +1,5 @@
-import * as $ from 'jquery';
+declare var jQuery: JQueryStatic;
+declare var $: JQueryStatic;
 
 module OakCMS {
 
